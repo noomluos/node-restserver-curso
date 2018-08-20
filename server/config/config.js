cbaacf55816860625892acf80d065861a8ee9ac8
Soulmoon <iscmdln@gmail.com>
2018-08-20 13:27:09 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb://cafe-user:123456asd@ds127342.mlab.com:27342/cafe-soulmoon"
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
